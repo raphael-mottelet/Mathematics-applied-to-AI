a433c467cc053aa85f344865bce73024a5b7821e
raphael-mottelet <72501205+raphael-mottelet@users.noreply.github.com>
2024-10-31 17:28:44 +0100

    adding gradient algorythm

--- test.py
+++ test.py
@@ -9,3 +9,24 @@ def function(x):
 x = np.arange(-3.0,3.0,0.01)
 y=function(x)
 plt.plot(x,y,'r-')
+
+def functionprime(x):
+    return 8*x+3
+
+#### gradient algorythm
+
+etq = 0.1
+eps= 0.0001
+x0=2
+
+plt.scatter (x0,functionprime(x0))
+print (x0, function(x))
+
+cond = abs(functionprime(x0))
+
+while cond > eps:
+    x0=x0 - etq*functionprime
+    y0= function(x0)
+    plt.scatter(x0,y0)
+    cond= abs(functionprime(x0))
+    
